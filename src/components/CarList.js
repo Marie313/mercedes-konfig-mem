f54b0432549fffb1cc03d5cbcc6d4492bc1630bd
Marie Zielke <marie.zielke@telekom.de>
2024-05-03 13:23:06 +0200

    showMoreCars

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,67 +1,116 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
-const CarList = ({selectedModell}) => {
+const CarList = ({ selectedModell }) => {
   const [car, setCar] = useState([]);
   const [showMoreCars, setShowMoreCars] = useState(false);
+  const [carback, setCarback] = useState([]);
+  const [buttonValues, setButtonValues] = useState({});
+
+  useEffect(() => {
+    const initialButtonValues = {};
+    for (let i = 0; i < 25; i++) {
+      initialButtonValues[i] = ">";
+    }
+    setButtonValues(initialButtonValues);
+  }, []);
 
   useEffect(() => {
     const fetchData = async () => {
-        const carData = [];
-        for (let i = 0; i < 25; i++) {
-          const response = await fetch(`http://localhost:8001/${i}`);
-          const data = await response.json();
-          carData.push(data);
-        }
-        setCar(carData);
+      const carData = [];
+      for (let i = 0; i < 25; i++) {
+        const response = await fetch(`http://localhost:8001/${i}`);
+        const data = await response.json();
+        carData.push(data);
+      }
+      setCar(carData);
+      setCarback(carData);
     };
-  
+
     fetchData();
   }, []);
 
-  const moreCars = (carsmoreCars) => {
+  const moreCars = (carsmoreCars, carsSuperiorClass, carsid) => {
+    const toggleButtonValue = () => {
+      setButtonValues((prevState) => ({
+        ...prevState,
+        [carsid]: prevState[carsid] === ">" ? "<" : ">",
+      }));
+    };
+
     const toggleShowMoreCars = () => {
       setShowMoreCars(!showMoreCars);
     };
-  
+
     const ShowCars = async () => {
-      toggleShowMoreCars(); // Zustand aktualisieren
+      toggleShowMoreCars();
       if (!showMoreCars) {
         const carMoreData = [];
         for (let i = 25; i < 41; i++) {
           const response = await fetch(`http://localhost:8001/${i}`);
           const data = await response.json();
-          carMoreData.push(data);
+          if (data.superiorClass === carsSuperiorClass) {
+            carMoreData.push(data);
+          }
         }
-        setCar(car.concat(carMoreData));
-        toggleShowMoreCars(); // Zustand aktualisieren
-      }
-      else {
-        // Wenn showMoreCars false ist, entferne die zusätzlichen Daten
-        setCar(car.slice(0, 25));
+        const addition = (carsid) => {
+          let add = 0;
+          if (carsid % 3 !== 0) {
+            carsid = carsid + 1;
+            if (carsid % 3 !== 0) {
+              carsid = carsid + 1;
+              add = 2;
+            } else {
+              add = 1;
+            }
+          }
+          return add;
+        };
+        setCar([
+          ...car.slice(0, carsid + addition(carsid)),
+          ...carMoreData,
+          ...car.slice(carsid + addition(carsid)),
+        ]);
+        toggleButtonValue(); // Buttonwert basierend auf dem ButtonId wechseln
+        toggleShowMoreCars();
+      } else {
+        setCar(carback);
+        toggleButtonValue(); // Buttonwert basierend auf dem ButtonId wechseln
+        toggleShowMoreCars();
       }
     };
-  
+
     if (carsmoreCars) {
       return (
-        <button className="moreButton" onClick={ShowCars}>v</button>
+        <button className="moreButton" onClick={ShowCars}>
+          {buttonValues[carsid]}
+        </button>
       );
     }
     return null;
   };
 
-  const showReturn = (carsMietModelle, carsKaufModelle, carspic, carsClass, carsmoreCars, carsid) => {
-      if (selectedModell==="mietmodell"){
-      	if (carsMietModelle === true){
-          if (carsmoreCars) return (
+  const showReturn = (
+    carsMietModelle,
+    carsKaufModelle,
+    carspic,
+    carsClass,
+    carsmoreCars,
+    carsid,
+    carsSuperiorClass
+  ) => {
+    if (selectedModell === "mietmodell") {
+      if (carsMietModelle === true) {
+        if (carsmoreCars)
+          return (
             <div className="showReturn">
               <img src={carspic} className="carsPic" />
               <div className="carClassDiv">
                 <p className="carClass">{carsClass}</p>
-                {moreCars(carsmoreCars)}
+                {moreCars(carsmoreCars, carsSuperiorClass, carsid)}
               </div>
             </div>
-          )
+          );
         return (
           <Link className="linkSpecificCar" to={`/specificCar/${carsid}`}>
             <div className="showReturn">
@@ -71,50 +120,51 @@ const CarList = ({selectedModell}) => {
               </div>
             </div>
           </Link>
-        );
-        }
-        return (
-          <div className="showReturnFalse">
-            <img src={carspic} className="carsPic" />
-            <div className="carClassDiv">
-              <p className="carClass">{carsClass}</p>
-              {moreCars(carsmoreCars)}
-            </div>
-          </div>
         );
       }
-      if (selectedModell==="kaufmodell"){
-      	if (carsKaufModelle === true){
-          if (carsmoreCars) return (
-            <div className="showReturn">
-              <img src={carspic} className="carsPic" />
-              <div className="carClassDiv">
-                <p className="carClass">{carsClass}</p>
-                {moreCars(carsmoreCars)}
-              </div>
-            </div>
-          )
-        return (
-          <Link className="linkSpecificCar" to={`/specificCar/${carsid}`}>
-            <div className="showReturn">
-              <img src={carspic} className="carsPic" />
-              <div className="carClassDiv">
-                <p className="carClass">{carsClass}</p>
-              </div>
-            </div>
-          </Link>
-        );
-        }
-        return (
+      return (
         <div className="showReturnFalse">
           <img src={carspic} className="carsPic" />
           <div className="carClassDiv">
             <p className="carClass">{carsClass}</p>
-            {moreCars(carsmoreCars)}
+            {moreCars(carsmoreCars, carsSuperiorClass, carsid)}
           </div>
         </div>
+      );
+    }
+    if (selectedModell === "kaufmodell") {
+      if (carsKaufModelle === true) {
+        if (carsmoreCars)
+          return (
+            <div className="showReturn">
+              <img src={carspic} className="carsPic" />
+              <div className="carClassDiv">
+                <p className="carClass">{carsClass}</p>
+                {moreCars(carsmoreCars, carsSuperiorClass, carsid)}
+              </div>
+            </div>
+          );
+        return (
+          <Link className="linkSpecificCar" to={`/specificCar/${carsid}`}>
+            <div className="showReturn">
+              <img src={carspic} className="carsPic" />
+              <div className="carClassDiv">
+                <p className="carClass">{carsClass}</p>
+              </div>
+            </div>
+          </Link>
         );
-        }
+      }
+      return (
+        <div className="showReturnFalse">
+          <img src={carspic} className="carsPic" />
+          <div className="carClassDiv">
+            <p className="carClass">{carsClass}</p>
+            {moreCars(carsmoreCars, carsSuperiorClass, carsid)}
+          </div>
+        </div>
+      );
+    }
   };
 
   return (
@@ -122,7 +172,15 @@ const CarList = ({selectedModell}) => {
       <div className="cars-map">
         {car.map((cars) => (
           <div className="cars-prview" key={cars.id}>
-            {showReturn(cars.mietmodell, cars.kaufmodell , cars.pic, cars.class, cars.moreCars, cars.id)}
+            {showReturn(
+              cars.mietmodell,
+              cars.kaufmodell,
+              cars.pic,
+              cars.class,
+              cars.moreCars,
+              cars.id,
+              cars.superiorClass
+            )}
           </div>
         ))}
       </div>
--- src/components/TypeList.js
+++ src/components/TypeList.js
@@ -1,78 +1,176 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
-const TypeList = ({selectedModell}) => {
-    const [type, setType] = useState([]);
+const TypeList = ({ selectedModell }) => {
+  const [type, setType] = useState([]);
+  const [showMoreCars, setShowMoreCars] = useState(false);
+  const [typeback, setTypeback] = useState([]);
+  const [buttonValues, setButtonValues] = useState({});
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const typeData = [];
-            for (let i = 0; i < 7; i++) {
-            const response = await fetch(`http://localhost:8002/${i}`);
-            const data = await response.json();
-            typeData.push(data);
-            }
-            setType(typeData);
-        };
-    
-        fetchData();
-    }, []);
+  useEffect(() => {
+    const initialButtonValues = {};
+    for (let i = 0; i < 25; i++) {
+      initialButtonValues[i] = ">";
+    }
+    setButtonValues(initialButtonValues);
+  }, []);
 
-    const showReturn = (typesMietmodell, typesKaufmodell, typespic, typesName) => {
-          if (selectedModell === "mietmodell")  {
-            if (typesMietmodell === true) return (
-                <div className="showReturn">
-                    <img src={typespic} className="carsPic" />
-                    <div className="carClassDiv">
-                        <p className="carClass">{typesName}</p>
-                        <button className="moreButton">v</button>
-                    </div>
-                </div>
-            );
-            return (
-            <div className="showReturnFalse">
-                <img src={typespic} className="carsPic" />
-                <div className="carClassDiv">
-                    <p className="carClass">{typesName}</p>
-                    <button className="moreButton">v</button>
-                </div>
-            </div>
-            );
-          }
-          if (selectedModell === "kaufmodell")  {
-            if (typesKaufmodell === true) return (
-                <div className="showReturn">
-                    <img src={typespic} className="carsPic" />
-                    <div className="carClassDiv">
-                        <p className="carClass">{typesName}</p>
-                        <button className="moreButton">v</button>
-                    </div>
-                </div>
-            );
-            return (
-            <div className="showReturnFalse">
-                <img src={typespic} className="carsPic" />
-                <div className="carClassDiv">
-                    <p className="carClass">{typesName}</p>
-                    <button className="moreButton">v</button>
-                </div>
-            </div>
-            );
-          }
+  useEffect(() => {
+    const fetchData = async () => {
+      const typeData = [];
+      for (let i = 0; i < 8; i++) {
+        const response = await fetch(`http://localhost:8002/${i}`);
+        const data = await response.json();
+        typeData.push(data);
+      }
+      setTypeback(typeData);
+      setType(typeData);
     };
 
-    return (
-        <div className="returnALL">
-        <div className="cars-map">
-            {type.map((types) => (
-            <div className="cars-prview" key={types.id}>
-                <button>
-                {showReturn(types.mietmodell, types.kaufmodell, types.pic_t, types.name)}
-                </button>
+    fetchData();
+  }, []);
+
+  const showReturn = (
+    typesMietmodell,
+    typesKaufmodell,
+    typespic,
+    typesclass,
+    typesid,
+    carsmoreCars
+  ) => {
+    if (selectedModell === "mietmodell") {
+      if (typesMietmodell === true) {
+        if (carsmoreCars)
+          return (
+            <div className="showReturn">
+              <img src={typespic} className="carsPic" />
+              <div className="carClassDiv">
+                <p className="carClass">{typesclass}</p>
+                {moreCars(carsmoreCars, typesid, typesclass)}
+              </div>
             </div>
-            ))}
+          );
+        return (
+          <Link className="linkSpecificCar" to={`/specificCar/${typesid}`}>
+            <div className="showReturn">
+              <img src={typespic} className="carsPic" />
+              <div className="carClassDiv">
+                <p className="carClass">{typesclass}</p>
+              </div>
+            </div>
+          </Link>
+        );
+      }
+      return (
+        <div className="showReturnFalse">
+          <img src={typespic} className="carsPic" />
+          <div className="carClassDiv">
+            <p className="carClass">{typesclass}</p>
+            {moreCars(carsmoreCars, typesid, typesclass)}
+          </div>
         </div>
+      );
+    }
+    if (selectedModell === "kaufmodell") {
+      if (typesKaufmodell === true) {
+        if (carsmoreCars)
+          return (
+            <div className="showReturn">
+              <img src={typespic} className="carsPic" />
+              <div className="carClassDiv">
+                <p className="carClass">{typesclass}</p>
+                {moreCars(carsmoreCars, typesid, typesclass)}
+              </div>
+            </div>
+          );
+        return (
+          <Link className="linkSpecificCar" to={`/specificCar/${typesid}`}>
+            <div className="showReturn">
+              <img src={typespic} className="carsPic" />
+              <div className="carClassDiv">
+                <p className="carClass">{typesclass}</p>
+                {moreCars(carsmoreCars, typesid, typesclass)}
+              </div>
+            </div>
+          </Link>
+        );
+      }
+      return (
+        <div className="showReturnFalse">
+          <img src={typespic} className="carsPic" />
+          <div className="carClassDiv">
+            <p className="carClass">{typesclass}</p>
+            {moreCars(carsmoreCars, typesid, typesclass)}
+          </div>
         </div>
-    );
+      );
+    }
+  };
+
+  const moreCars = (carsmoreCars, typesid, typeskarosserie) => {
+    const toggleButtonValue = () => {
+      setButtonValues((prevState) => ({
+        ...prevState,
+        [typesid]: prevState[typesid] === ">" ? "<" : ">",
+      }));
     };
+
+    const toggleShowMoreCars = () => {
+      setShowMoreCars(!showMoreCars);
+    };
+
+    const ShowCars = async () => {
+      if (!showMoreCars) {
+        const carMoreData = [];
+        for (let i = 0; i < 41; i++) {
+          const response = await fetch(`http://localhost:8001/${i}`);
+          const data = await response.json();
+          if (data.karosserie === typeskarosserie) {
+            carMoreData.push(data);
+          }
+        }
+        setType([
+          ...type.slice(0, typesid),
+          ...carMoreData,
+          ...type.slice(typesid),
+        ]);
+        toggleShowMoreCars();
+        toggleButtonValue();
+        return <div className="newCars"></div>;
+      } else {
+        setType(typeback);
+        toggleButtonValue();
+        toggleShowMoreCars();
+      }
+    };
+    if (carsmoreCars) {
+      return (
+        <button className="moreButton" onClick={ShowCars}>
+          {buttonValues[typesid]}
+        </button>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <div className="returnALL">
+      <div className="cars-map">
+        {type.map((types) => (
+          <div className="cars-prview" key={types.id}>
+            {showReturn(
+              types.mietmodell,
+              types.kaufmodell,
+              types.pic,
+              types.class,
+              types.id,
+              types.moreCars
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default TypeList;
